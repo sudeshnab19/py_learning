c3ba5ca1930adcbeed08b50a636b4024484104eb
Sudeshna B <sudeshnab@Sudeshnas-Air.Dlink>
2024-01-20 22:25:21 +0530

    python functions

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+# THis is how we write comments
+# function definition 
+def sum(a, b):
+    """
+    Function to add two numbers
+    """
+    return a+b
+
+def subtract(a, b): 
+    return a-b
+
+def multiply(a, b): 
+    return a*b
+
+def divide(a, b): 
+    return a/b
+
+'''
+Hell this is multiline comment
+Sudeshna is coding here!!
+'''
+
+print("Doing sum:",sum(4,20)," Function call done")
+print("Doing subtract:",subtract(20,4)," Function call done")
+print("Doing multiply:",multiply(20,4),"Function calling done")
+print("Doing divide:",divide(20,4),"Function calling done")
+print("Hello, Sudeshna is back in coding finally!!")
+ 
---
+++ notes.md
@@ -0,0 +1,5 @@
+# Learning notes
+
+## Set up virtual environment 
+* `python3 /path/to/venv`
+* `source /path/to/venv/bin/activate`

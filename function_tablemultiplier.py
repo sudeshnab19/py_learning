9eb5e37c4b8e86ae197da4e67b83919f5854e846
Sudeshna B <sudeshnab@Sudeshnas-Air.Dlink>
2024-03-04 17:50:43 +0530

    Table Multiplier

---
+++ function_tablemultiplier.py
@@ -0,0 +1,30 @@
+""""
+def average(a,b): 
+    avg=(a+b)/2
+    print(avg)
+a: int=8
+b: int=9
+average(a,b)
+"""
+
+""""
+x=int(input("please enter the table multiplier: "))
+y=int(input("please mention the no. of table multipliers: "))
+i=1
+while i<=y:
+    val=(x*i)
+    print(val)
+    i+=1
+"""
+
+def table_multiplier(i,x,y):
+    while i<=y:
+        val=(x*i)
+        print(val)
+        i+=1
+
+x=int(input("please enter the table multiplier: "))
+y=int(input("please mention the no. of table multipliers: "))
+i=1
+table_multiplier(i,x,y)
+    
